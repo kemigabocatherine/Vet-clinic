1fe9fd713bd747186f75b628f60c73c9cf6bedf8
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-06-01 23:46:38 +0300

    Add data to the table animals

--- data.sql
+++ data.sql
@@ -1,5 +1,14 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (1, 'Agumon', 'Feb 3,2020', 0, TRUE, 10.23);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (2, 'Gabumon', 'Nov 15,2018', 2, TRUE, 8);
+
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (3, 'Pikachu', 'Jan 7,2021', 1, FALSE, 15.04);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (4, 'Devimon', 'May 12,2017', 5, TRUE, 11);
