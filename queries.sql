d52d136144469491ef3c06cf3a4f331173519f04
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-06-03 22:43:41 +0300

    Use the 'JOIN' to write queries and answer some questions

--- queries.sql
+++ queries.sql
@@ -64,3 +64,56 @@ SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth > '1990-12-31' AND date_of_birth < '2000-01-01'
 GROUP BY species;
+
+/* Animals owned by Melody */
+
+SELECT name FROM animals
+JOIN owners
+ON owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+/* Animals of pokemon type */
+
+SELECT animals.name FROM animals
+JOIN species
+ON species_id = species.id
+WHERE species.id = 1;
+
+/* All animals and their owners */
+
+SELECT animals.name, owners.full_name FROM animals
+FULL JOIN owners
+ON owner_id = owners.id;
+
+/* Animals per specie */
+
+SELECT species.name, COUNT(*) FROM animals
+JOIN species
+ON species_id = species.id
+GROUP BY species.id;
+
+/* Digimon owned by Jennifer Orwell */
+
+SELECT animals.name FROM animals
+JOIN owners
+ON owner_id = owners.id
+JOIN species
+ON species_id = species.id
+WHERE species.id = '2' AND owners.full_name = 'Jennifer Orwell';
+
+/* Animals owned by Dean Winchester with O escape_attempts */
+
+SELECT animals.name FROM animals
+JOIN owners
+ON owner_id = owners.id
+WHERE owners.full_name = 'Dean Winchester' AND escape_attempts = 0;
+
+/* Owns the most aniamls */
+SELECT * FROM animals;
+SELECT owners.full_name FROM animals
+JOIN owners
+ON owner_id = owners.id
+GROUP BY owners.full_name
+ORDER BY COUNT(owner_id) DESC
+LIMIT 1;
+
